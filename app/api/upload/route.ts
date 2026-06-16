5aaba8983d82a122bff88c8446dbb0b7be7c3552
FrieTi220044 <40146720220044@litec.ac.at>
2026-06-16 12:10:48 +0200

    feat: add logging for uploaded files in upload routes

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,6 @@
 import { RejectUpload, type Router, route } from "@better-upload/server";
 import { toRouteHandler } from "@better-upload/server/adapters/next";
+import { Console } from "console";
 import { headers } from "next/headers";
 import { env } from "@/env";
 import { auth } from "@/lib/auth";
@@ -20,6 +21,10 @@ const router: Router = {
                     throw new RejectUpload("unauthorized");
                 }
             },
+
+            onAfterSignedUrl: async ({ file }) => {
+                console.log(file.objectInfo.key);
+            },
         }),
         projectImages: route({
             fileTypes: ["image/*"],
@@ -31,6 +36,13 @@ const router: Router = {
 
                 if (!session) {
                     throw new RejectUpload("unauthorized");
+                }
+            },
+
+            onAfterSignedUrl: async ({ files }) => {
+                // the files now have the objectInfo property
+                for (const file of files) {
+                    console.log("File uploaded:", file.objectInfo.key);
                 }
             },
         }),
