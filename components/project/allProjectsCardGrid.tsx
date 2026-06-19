e07f7d1178713c624767175ff0b797d1d5e2c0ed
black-wulf <emanlatic10@gmail.com>
2026-06-19 16:36:11 +0200

    fix: added revalidate

--- components/project/allProjectsCardGrid.tsx
+++ components/project/allProjectsCardGrid.tsx
@@ -1,6 +1,8 @@
 import { GetAllProjects } from "@/lib/connected-db-oc/project";
 import { ProjectCardGrid } from "./projectCardGrid";
 
+export const revalidate = 3600;
+
 export default async function AllProjectsCardGrid() {
     const projects = await GetAllProjects();
 
