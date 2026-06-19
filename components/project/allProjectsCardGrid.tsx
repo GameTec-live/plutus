f7c0865053954c238b3062f1110c36ee1f4110fe
black-wulf <emanlatic10@gmail.com>
2026-06-19 15:51:02 +0200

    fix: added cache to correct component

--- components/project/allProjectsCardGrid.tsx
+++ components/project/allProjectsCardGrid.tsx
@@ -1,12 +1,7 @@
-import { cacheLife, cacheTag } from "next/cache";
 import { GetAllProjects } from "@/lib/connected-db-oc/project";
 import { ProjectCardGrid } from "./projectCardGrid";
 
 export default async function AllProjectsCardGrid() {
-    "use cache";
-    cacheLife("days");
-    cacheTag("projectGrid");
-
     const projects = await GetAllProjects();
 
     return <ProjectCardGrid projects={projects} />;
